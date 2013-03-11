32a5cc3415677065aa85fb684ac9a4c544811d81
Harry Goo <guhhjj@gmail.com>
2013-03-11 22:07:59 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuhhjjBlog::Application.config.secret_token = '3784c054ff63f30e3dbf0b2b889ff35bd6d7ee0193e146908371b9190536aec7eaeda2ecc50464db07e359777144fde72e32f4ce6431451405a80a4155286736'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GuhhjjBlog::Application.config.session_store :cookie_store, key: '_guhhjj-blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GuhhjjBlog::Application.config.session_store :active_record_store
