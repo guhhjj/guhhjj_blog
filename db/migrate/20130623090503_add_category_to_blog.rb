bb1724ef638ebba5db177f850c5a04ca24b2ca83
guhhjj <guhhjj@gmail.com>
2013-10-20 07:46:28 +0800

    delete some temp files

--- db/migrate/20130623090503_add_category_to_blog.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToBlog < ActiveRecord::Migration
-  def change
-    add_column :blogs, :category_id, :integer
-  end
-end
--- db/migrate/20130824032131_add_column_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToComment < ActiveRecord::Migration
-  def change
-    add_column :comments, :nickname, :string
-  end
-end
